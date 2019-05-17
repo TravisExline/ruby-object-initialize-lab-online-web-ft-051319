701d27a3d29be75cffe86aa41558c19f20b24ea9
Travis Exline <t.exline22@gmail.com>
2019-05-17 14:59:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
   end
 end
